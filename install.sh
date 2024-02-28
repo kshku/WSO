a0cf61cb6e3eb8cf39238c6af08cd2ff580a3978
kshku <1211shree@gmail.com>
2024-02-28 22:10:31 +0530

    Removed sudo

--- install.sh
+++ install.sh
@@ -4,14 +4,14 @@ script_url="https://raw.githubusercontent.com/kshku/WSO/main/wso.sh"
 install_dir="/usr/local/bin"
 
 if command -v curl > /dev/null; then
-    sudo curl -sSLo "$install_dir/wso" "$script_url"
+    curl -sSLo "$install_dir/wso" "$script_url"
 elif command -v wget > /dev/null; then
-    sudo wget -qO "$install_dir/wso" "$script_url"
+    wget -qO "$install_dir/wso" "$script_url"
 else
     echo "Error: Neither curl nor wget found. Please install one of them to proceed"
     exit 1
 fi
 
-sudo chmod +x "$install_dir/wso"
+chmod +x "$install_dir/wso"
 
 echo "wso has been installed to $install_dir"
