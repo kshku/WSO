028f9fe10b332273193bfc028f55665df7f1b0eb
kshku <1211shree@gmail.com>
2024-02-28 22:05:22 +0530

    Refractored
    remvoed the executable permission from wso.sh it is handled during
    installation
    modified install.sh to use sudo

--- install.sh
+++ install.sh
@@ -4,9 +4,9 @@ script_url="https://raw.githubusercontent.com/kshku/WSO/main/wso.sh"
 install_dir="/usr/local/bin"
 
 if command -v curl > /dev/null; then
-    curl -sSLo "$install_dir/wso" "$script_url"
+    sudo curl -sSLo "$install_dir/wso" "$script_url"
 elif command -v wget > /dev/null; then
-    wget -qO "$install_dir/wso" "$script_url"
+    sudo wget -qO "$install_dir/wso" "$script_url"
 else
     echo "Error: Neither curl nor wget found. Please install one of them to proceed"
     exit 1
